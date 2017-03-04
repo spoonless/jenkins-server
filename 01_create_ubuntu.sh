c38f3f26ba1dee6a06c917cd9a4a47145c696874
David Gayerie <dagaydevel@free.fr>
2017-03-04 15:52:53 +0100

    utilisation des scripts lxc dans la branche lxc

--- 01_create_ubuntu.sh
+++ 01_create_ubuntu.sh
@@ -7,18 +7,19 @@ do
   read -p "Donnez le nom du nouveau conteneur: " CONTAINER_NAME
 done
 
-if [ $(lxc list -c n | grep -c "| *$CONTAINER_NAME *|") == 1 ]
+if [ $(sudo lxc-ls -1 | grep -c "^$CONTAINER_NAME$") == 1 ]
 then
   echo "Un conteneur porte déja ce nom..."
 else
-  lxc launch images:ubuntu/xenial/amd64 "$CONTAINER_NAME"
-  sleep 5
+  sudo lxc-create --template download --name "$CONTAINER_NAME" -- -d ubuntu -r xenial -a amd64
 fi
 
-lxc exec "$CONTAINER_NAME" -- apt update
+sudo lxc-start -n "$CONTAINER_NAME"
 sleep 5
-lxc exec "$CONTAINER_NAME" -- apt -y upgrade
-lxc exec "$CONTAINER_NAME" -- apt -y install openssh-server python sudo
-lxc exec "$CONTAINER_NAME" -- adduser $USER
-lxc exec "$CONTAINER_NAME" -- usermod -a -G sudo $USER
-lxc restart "$CONTAINER_NAME"
+sudo lxc-attach -n "$CONTAINER_NAME" -- apt update
+sleep 5
+sudo lxc-attach -n "$CONTAINER_NAME" -- apt -y upgrade
+sudo lxc-attach -n "$CONTAINER_NAME" -- apt -y install openssh-server python sudo
+sudo lxc-attach -n "$CONTAINER_NAME" -- adduser $USER
+sudo lxc-attach -n "$CONTAINER_NAME" -- usermod -a -G sudo $USER
+
